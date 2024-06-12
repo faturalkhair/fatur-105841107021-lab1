2f21c09bfc8d734fa39532212804f0fe673451ee
faturalkhair <105841107021@student.unismuh.ac.id>
2024-06-12 23:53:22 +0800

    Button Component

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StyleSheet, Text, View, Image} from 'react-native'
 import React from 'react'
 
-
 const App = () => {
+  const ButtonCostum =({color, text}) => {
+    return(
+      <View style={{
+        backgroundColor: color, width: 150, height: 70,
+        justifyContent: 'center',
+        borderRadius: 10,
+        margin: 20
+      }}>
+        <Text style={{
+          fontSize: 20, color: 'white',
+          borderRadius: 20,
+          textAlign: 'center',
+          fontWeight: 'bold'
+        }}>
+          {text}
+        </Text>
+      </View>
+      
+    )
+  }
   return (
-
     <View style={{flex:1}}>
       <View style={{
         justifyContent: 'center',
@@ -34,39 +52,11 @@ const App = () => {
         flex:1, flexDirection: "row",
         alignItems: 'flex-end',
         justifyContent: 'center',
-        marginBottom: 30
+        marginBottom: 30,
       }}>
-
-    <View style={{
-      backgroundColor: 'red', width: 150, height: 70,
-      justifyContent: 'center',
-      borderRadius: 10
-    }}>
-      <Text style={{
-        fontSize: 20, color: 'white',
-        borderRadius: 20,
-        textAlign: 'center',
-        fontWeight: 'bold'
-      }}>
-        LOGIN
-      </Text>
-    </View>
-      <View style={{
-        alignItems:'center',
-        justifyContent: 'center',
-        backgroundColor: 'blue', width: 150, height: 70,
-        borderRadius: 10,
-        marginLeft: 10
-      }}>
-        <Text style={{
-          fontSize: 20, color: 'white',
-          borderRadius: 20,
-          textAlign: 'center',
-          fontWeight: 'bold'
-        }}>
-          LOGOUT
-        </Text>
-      </View>     
+        <ButtonCostum color='red' text='LOGIN'/>
+        <ButtonCostum color='blue' text='LOGOUT'/>
+ 
       </View>
     </View>
 
