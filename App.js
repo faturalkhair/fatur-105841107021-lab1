453a2247bf9f2f5a78726e2936c9645e2e8f18af
faturalkhair <105841107021@student.unismuh.ac.id>
2024-06-25 03:05:39 +0800

    signUp

--- App.js
+++ App.js
@@ -2,41 +2,11 @@ import { StyleSheet, Text, View, Image, TextInput} from "react-native"
 import React from "react"
  
 const App = () => {
-  const TextInputCustom =({name, color}) =>{
-    return(
-      <TextInput
-      placeholder={`Masukkan ${name}`}
-      style={{
-        height: 40,
-        width: 200,
-        borderColor: 'green',
-        borderWidth: 2,
-        padding: 10,
-        margin: 10,
-        borderRadius: 10,
-        color: color,
-      }}
-    />
-
-    )
-  }
-  return (
-    <View style={{
-      flex:1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-
-    <TextInputCustom name="Username" color="red"/>
-    <TextInputCustom name="Password" color="yellow"/>
+ return(
+  
+ )
  
 
-
-
-    </View>
-
-  )
 }
-
 export default App
 
