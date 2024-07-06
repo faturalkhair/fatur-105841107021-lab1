98dc35e89513c8e3f8bc7d17dd7e67b50ba339f9
faturalkhair <105841107021@student.unismuh.ac.id>
2024-07-07 02:22:16 +0800

    Navigation & Taskbar :v

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StyleSheet, Text, View, Image, TextInput} from "react-native"
-import React from "react"
-import { useFonts } from 'expo-font'
-  
-const App = () => { 
- 
- 
+// In App.js in a new project
 
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginPage from './LoginPage';
+import { Button } from 'react-native-web';
+
+
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title="KE HALAMAN LOGIN" onPress={() => navigation.navigate('LoginPage')}/>
+    </View>
+  );
 }
-export default App
 
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="LoginPage" component={LoginPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+// maaf kak terlambat ku commit, bermasalah kemarin laptop ku. nda bisa ta connect internet
