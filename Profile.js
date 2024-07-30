3c94ed91a7576a357da77e36fa2a1c4f3b5da130
faturalkhair <105841107021@student.unismuh.ac.id>
2024-07-31 00:14:54 +0800

    Halaman Profile

---
+++ Profile.js
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <View style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '50%'
+            
+    
+        }}>
+          <Text style={{
+            fontSize: 20,
+            fontWeight: 'bold'
+    
+          }}>
+            INI HALAMAN PROFILE</Text>
+        </View>
+      )
+    }
+
+export default Profile
