ddc35c351fe8dc6189cf6d9b2d36285367701582
faturalkhair <105841107021@student.unismuh.ac.id>
2024-07-31 00:13:09 +0800

    Halaman Favorites

---
+++ Favorites.js
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Favorites = () => {
+    return (
+        <View style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '50%'
+            
+    
+        }}>
+          <Text style={{
+            fontSize: 20,
+            fontWeight: 'bold'
+    
+          }}>
+            INI HALAMAN FAVORITES</Text>
+        </View>
+      )
+    }
+export default Favorites
