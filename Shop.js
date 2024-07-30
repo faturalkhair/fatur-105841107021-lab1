8aac740b94ab667e878ebc2e2c4df48847de26c5
faturalkhair <105841107021@student.unismuh.ac.id>
2024-07-31 00:11:08 +0800

    Halaman Shop

---
+++ Shop.js
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <View style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '50%'
+        
+
+    }}>
+      <Text style={{
+        fontSize: 20,
+        fontWeight: 'bold'
+
+      }}>
+        INI HALAMAN SHOP</Text>
+    </View>
+  )
+}
+export default Shop
